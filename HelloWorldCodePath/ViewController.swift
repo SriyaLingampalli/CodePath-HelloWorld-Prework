fa4a91e96b6e1f8a1dd7e3d47dd4e9735ff6a982
SriyaLingampalli <36239050+SriyaLingampalli@users.noreply.github.com>
2018-09-04 12:42:45 -0700

    Add files via upload

---
+++ HelloWorldCodePath/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  HelloWorldCodePath
+//
+//  Created by Sriya Lingampalli on 8/29/18.
+//  Copyright © 2018 Sriya Lingampalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var backgroundColor: UIColor!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        backgroundColor = view.backgroundColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func didTapButton(_ sender: Any) {
+        print("Hello")
+        textLabel.textColor = UIColor.brown
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+    }
+    @IBAction func didTapTextButton(_ sender: Any) {
+        //textLabel.text = "Goodbye!"
+        textLabel.text = textField.text
+        textField.text = ""
+        view.endEditing(true)
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLabel.text = "Hello"
+        textField.text = ""
+        view.backgroundColor = backgroundColor
+        textLabel.textColor = UIColor.black
+    }
+}
+
